8741767edfd9cbff74a60ef112137c0d831495c4
Madara Uchiha <izidyzman@gmail.com>
2022-05-01 23:27:52 +0300

    Добавьте файлы проекта.

---
+++ 8 lab prac/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace _8_lab_prac
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Запустить = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Запустить
+            // 
+            this.Запустить.Location = new System.Drawing.Point(12, 200);
+            this.Запустить.Name = "Запустить";
+            this.Запустить.Size = new System.Drawing.Size(395, 23);
+            this.Запустить.TabIndex = 1;
+            this.Запустить.Text = "Запустить";
+            this.Запустить.UseVisualStyleBackColor = true;
+            this.Запустить.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(183, 229);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(31, 27);
+            this.textBox1.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeight = 5;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 16);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 5;
+            this.dataGridView1.Size = new System.Drawing.Size(178, 178);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(203, 16);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(201, 178);
+            this.dataGridView2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Исходный";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(203, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Измененный ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 268);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Запустить);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Запустить;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ 8 lab prac/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _8_lab_prac
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.RowCount = 7;
+            dataGridView1.ColumnCount = 7;
+            dataGridView2.RowCount = 7;
+            dataGridView2.ColumnCount = 7;
+            int[,] A = new int[7, 7];
+            int i,j;
+            int imax=0,jmax=0;
+            int temp = 0;
+            Random rand = new Random();
+            for (i = 0; i < 7; i++)
+                for (j = 0; j < 7; j++)
+                    A[i, j] = rand.Next(-10, 10);
+            for (i = 0; i < 7; i++)
+                for (j = 0; j < 7; j++)
+                    dataGridView1.Rows[i].Cells[j].Value = A[i, j].ToString();
+            int max1 = A[0, 0]; 
+            for ( i = 0; i < A.GetLength(0); i++)
+            {
+                if (A[i, i] > max1)
+                {
+                    max1 = A[i, i];
+                    imax = i;
+                    jmax = i;
+                }   
+            }
+            for ( i = 0; i < A.GetLength(0); i++)
+            {  
+                    if ( A[i,A.GetLength(0)-1-i] > max1)
+                        max1 =  A[i,  A.GetLength(0) - 1-i];
+                imax = i;
+                jmax = i;
+            }
+            temp = A[A.GetLength(0) / 2, A.GetLength(0) / 2];
+            A[A.GetLength(0) / 2, A.GetLength(0) / 2] = max1;
+            A[imax,jmax]=temp;
+            for (i = 0; i < 7; i++)
+                for (j = 0; j < 7; j++)
+                    dataGridView2.Rows[i].Cells[j].Value = A[i, j].ToString();
+            textBox1.Text = Convert.ToString(max1);
+            dataGridView1.AutoResizeColumns();
+            dataGridView1.AutoResizeRows();
+            dataGridView2.AutoResizeColumns();
+            dataGridView2.AutoResizeRows();
+        }
+    }
+}
